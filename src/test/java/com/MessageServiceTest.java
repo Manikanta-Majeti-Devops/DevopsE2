f8f4d5234a9eb769a96ce060ca5d36865e76eeaa
Manikanta-Majeti-Devops <58879583+Manikanta-Majeti-Devops@users.noreply.github.com>
2020-08-01 13:00:01 +0100

    Uncomment the code

--- src/test/java/com/MessageServiceTest.java
+++ src/test/java/com/MessageServiceTest.java
@@ -1,29 +1,29 @@
-//Package com;
+Package com;
 
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-//import com.MessageService;
+import com.MessageService;
 
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest
-//public class MessageServiceTest {
-//
-//	@Test
-//	public void testMessage() {
-//		MessageService ms = new MessageService();
-//		System.out.println("hello");
-//		Assertions.assertEquals(ms.sayHello(), "hello");
-//	}
-//
-//	@Test
-//	public void testMessage2() {
-//		MessageService ms2 = new MessageService();
-//		System.out.println("hello world again");
-//		Assertions.assertEquals(ms2.sayHello(), "hello world");
-//	}
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class MessageServiceTest {
+
+	@Test
+	public void testMessage() {
+		MessageService ms = new MessageService();
+		System.out.println("hello");
+		Assertions.assertEquals(ms.sayHello(), "hello");
+	}
+
+	@Test
+	public void testMessage2() {
+		MessageService ms2 = new MessageService();
+		System.out.println("hello world again");
+		Assertions.assertEquals(ms2.sayHello(), "hello world");
+	}
 	
-//}
+}
