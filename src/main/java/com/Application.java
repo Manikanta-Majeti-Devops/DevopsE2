3ac485b9b7d73c3911d88bdb36c82fc113f7e3df
Manikanta-Majeti-Devops <58879583+Manikanta-Majeti-Devops@users.noreply.github.com>
2020-08-01 21:42:02 +0100

    Update Application.java

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-        private static final Logger log = LoggerFactory.getLogger(Application.class);
+        public static final Logger log = LoggerFactory.getLogger(Application.class);
 
         public static void main(String[] args) {
                 log.info("Main method has been invoked");
